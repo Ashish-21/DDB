34509e0eb1f962a8698f312559014da3ed2984e6
shubham9768 <shubhamgaikwad500@gmail.com>
2018-03-09 21:09:49 +0530

    forms added

---
+++ Details/personal.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+<br><br>
+<div class="container">
+  <div class="col-sm-offset-3 col-sm-7"> 
+  <div class="panel-group" id="accordion">
+  <div class="panel panel-danger">
+
+      <h2><div class="panel-heading">Personal Details</div></h2>
+      <div class="panel-body"><form class="form-horizontal" action="/action_page.php">
+    
+    <div class="form-group">
+      <label class="control-label col-sm-3" for="email">Name:</label>
+      <div class="col-sm-7">
+        <input type="email" class="form-control" id="email" placeholder="Enter Name" name="email">
+      </div>
+    </div>
+    
+    <div class="form-group">
+      <label class="control-label col-sm-3" for="email">Email:</label>
+      <div class="col-sm-7">
+        <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
+      </div>
+    </div>
+
+    <div class="form-group">
+      <label class="control-label col-sm-3" for="email">Mobile No:</label>
+      <div class="col-sm-7">
+        <input type="email" class="form-control" id="email" placeholder="Enter Mobile" name="email">
+      </div>
+    </div>
+
+    <div class="form-group">
+      <label class="control-label col-sm-3" for="email">Email:</label>
+      <div class="col-sm-7">
+        <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
+      </div>
+    </div>
+
+    <div class="form-group">
+      <label class="control-label col-sm-3" for="email">Email:</label>
+      <div class="col-sm-7">
+        <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
+      </div>
+    </div>
+
+    <div class="form-group">
+      <label class="control-label col-sm-3" for="email">Email:</label>
+      <div class="col-sm-7">
+        <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
+      </div>
+    </div>
+
+    <div class="panel panel-danger">
+      <div class="panel-heading">
+        <h4 class="panel-title">
+          <a data-toggle="collapse" data-parent="#accordion" href="#collapse1">Caste Details</a>
+        </h4>
+      </div>
+      <div id="collapse1" class="panel-collapse collapse">
+        <div class="panel-body">
+         
+          <div class="form-group">
+      <label class="control-label col-sm-3" for="email">Name:</label>
+      <div class="col-sm-7">
+        <input type="email" class="form-control" id="email" placeholder="Enter Name" name="email">
+      </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+   
+    <div class="panel panel-danger">
+      <div class="panel-heading">
+        <h4 class="panel-title">
+          <a data-toggle="collapse" data-parent="#accordion" href="#collapse2">Family Details</a>
+        </h4>
+      </div>
+      <div id="collapse2" class="panel-collapse collapse">
+        <div class="panel-body">
+
+           <div class="form-group">
+      <label class="control-label col-sm-3" for="email">Name:</label>
+      <div class="col-sm-7">
+        <input type="email" class="form-control" id="email" placeholder="Enter Name" name="email">
+      </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+    <div class="panel panel-danger">
+      <div class="panel-heading">
+        <h4 class="panel-title">
+          <a data-toggle="collapse" data-parent="#accordion" href="#collapse3">Income Details</a>
+        </h4>
+      </div>
+      <div id="collapse3" class="panel-collapse collapse">
+        <div class="panel-body">
+
+          <div class="form-group">
+      <label class="control-label col-sm-3" for="email">Name:</label>
+      <div class="col-sm-7">
+        <input type="email" class="form-control" id="email" placeholder="Enter Name" name="email">
+      </div>
+          </div>
+
+           <div class="form-group">        
+      <div class="col-sm-offset-3 col-sm-10">
+        <button type="submit" class="btn btn-default">Submit</button>
+      </div>
+      </div>
+
+        </div>
+      </div>
+    </div>
+
+</form>
+    </div>
+  </div>
+</div>
+</div>
+</div>
+</body>
+</html>
